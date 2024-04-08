8389aeb3b66d2783e127566cb42832b1c498701c
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-08 15:34:39 -0300

    Atividade resolvida!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Rolagem de Dado");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+int sorteou = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.WriteLine($"\nSorteou: {sorteou}");
+
